f3977264547de9fdb9ed3e2352b91f67d9d76b06
Tiago Costa <tiagodpv@gmail.com>
2024-01-09 21:32:46 -0500

    add return

--- hello.go
+++ hello.go
@@ -1,5 +1,7 @@
 package main
 
+/*--------------------RETORNO NORMAL-----------------------------------------
+
 import (
 	"errors"
 	"fmt"
@@ -20,7 +22,49 @@ func soma(x int, y int) (int, error) {
 
 	if res > 10 {
 		return 0, errors.New("Total maior que 10")
-	} 
+	}
 
 	return res, nil
+}*/
+
+/*----------------------RETORNO MULTIPLO------------------------------------------
+
+import "fmt"
+
+
+func main() {
+	resultado, str := soma(10, 20)
+
+	fmt.Println(resultado, str)
+}
+
+func soma(a int, b int) (int, string) {
+	return a + b, "somou!"
+}*/
+
+
+import "fmt"
+
+
+func main() {
+	//resultado := soma(10, 20)
+	resultado :=somaTudo(3,5,7,8)
+
+	fmt.Println(resultado)
+}
+
+func soma(a int, b int) (result int) { //retorno nomeado
+	result = a + b
+	return 
+}
+
+
+func somaTudo(x...int) int{
+	resultado :=0
+
+	for _, v :+ range x {
+		resultado += v
+	}
+
+	return resultado
 }
